a127323ed5774799f79368c086639abf01a4c522
Michael Braun <m.braun92@gmail.com>
2020-07-02 22:16:39 +0200

    Updates

---
+++ precache-manifest.27e936df54c31efae5268b9d1f862958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b63bb36571ed85bab27d98ecba98dc0",
+    "url": "/onryo/index.html"
+  },
+  {
+    "revision": "6afbdfd5c893bd0738f2",
+    "url": "/onryo/static/css/main.766126ab.chunk.css"
+  },
+  {
+    "revision": "5db8b2d9c5e1b091ebad",
+    "url": "/onryo/static/js/2.fd34fc83.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/onryo/static/js/2.fd34fc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afbdfd5c893bd0738f2",
+    "url": "/onryo/static/js/main.0ca50508.chunk.js"
+  },
+  {
+    "revision": "d4d6a4523ccd4f267d24",
+    "url": "/onryo/static/js/runtime-main.ef088966.js"
+  }
+]);
